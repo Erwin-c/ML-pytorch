0878ba75f18d483c2c4097542709f57d0fdbb404
Erwin <274868686@qq.com>
2021-05-09 20:37:10 +0800

    ...

---
+++ demo_attribute.py
@@ -0,0 +1,19 @@
+"""
+  demo_attribute.py
+
+    Created on: May 9, 2021
+        Author: Erwin
+"""
+
+import torch
+
+dev = torch.device("cpu")
+
+a = torch.tensor([2, 2], dtype=torch.float32, device=dev)
+print(a)
+
+i = torch.tensor([[0, 1, 2], [0, 1, 2]])
+v = torch.tensor([1, 2, 3])
+a = torch.sparse_coo_tensor(i, v, (4, 4), dtype=torch.float32, device=dev)
+# a = torch.sparse_coo_tensor(i, v, (4, 4)).to_dense()
+print(a)
