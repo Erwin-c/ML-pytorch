7333a9d1659f65e7cbf7316b91875aa9b9b015c4
Erwin <274868686@qq.com>
2021-05-21 07:00:25 +0800

    ...

--- op.py
+++ op.py
@@ -54,3 +54,62 @@ print(a)
 print(a.mul_(b))
 
 print(a)
+
+# div
+print("=== div res ===")
+print(a)
+print(b)
+
+print(a/b)
+print(a.div(b))
+print(torch.div(a, b))
+
+print(a)
+
+print(a.div_(b))
+
+print(a)
+
+# matmul
+print("=== matmul res ===")
+
+a = torch.ones(2, 1)
+b = torch.ones(1, 2)
+print(a)
+print(b)
+
+print(a @ b)
+print(a.matmul(b))
+print(a.mm(b))
+print(torch.matmul(a, b))
+print(torch.mm(a, b))
+
+# high dimension tensor
+print("=== high dimension res ===")
+
+a = torch.ones(1, 2, 3, 4)
+b = torch.ones(1, 2, 4, 3)
+print(a)
+print(b)
+
+print(a @ b)
+print(a.matmul(b))
+print(torch.matmul(a, b))
+
+print(torch.matmul(a, b).shape)
+
+# pow
+print("=== pow res ===")
+
+a = torch.tensor([1, 2])
+print(a)
+
+print(a**3)
+print(a.pow(3))
+print(torch.pow(a, 3))
+
+print(a)
+
+print(a.pow_(3))
+
+print(a)
